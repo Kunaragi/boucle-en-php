f85eff58f01df3f2368f9752a0db41903af0053d
Yohan Gopee <gopeeyohan@gmail.com>
2016-11-24 10:10:25 +0100

    Create exemple.php

---
+++ exemple.php
@@ -0,0 +1,56 @@
+<h1>Exo 1, petit 1</h1>
+
+<?php
+for ( $i=1 ; $i<101 ; $i++ ) {
+
+
+echo ($i) ;
+echo ("<hr>") ;
+
+
+}
+
+
+ ?>
+
+
+<h1>Exo 1, petit 2</h1>
+
+<?php
+$i=0 ;
+while ($i<100) {
+$i=$i+1;
+echo ($i);
+echo ("<hr>");
+
+}
+
+
+
+?>
+
+
+
+<h1>Exo 2, petit 1</h1>
+
+<?php
+for ( $i=100 ; $i>0 ; $i--) {
+echo ($i) ;
+echo ("<hr>") ;
+
+}
+
+
+ ?>
+
+ <h1>Exo 2, petit 2</h1>
+
+<?php
+$i=101 ;
+while ($i>0) {
+$i= $i-1;
+echo ($i) ;
+echo ("<hr>") ;
+}
+?>
+
